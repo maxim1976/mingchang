ea31ddae56662c04c01db4939500d95ce839a8da
maxim1976 <maximdongurool@gmail.com>
2025-12-11 15:57:21 +0800

    Update Railway deployment URL to demoshop.up.railway.app

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,11 +8,11 @@ from django.core.exceptions import ImproperlyConfigured
 
 DEBUG = env('DEBUG', default=False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['mingchang-meat.railway.app', 'mingchang-production.up.railway.app'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['demoshop.up.railway.app'])
 
 # CSRF trusted origins for Railway deployment
 CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[
-    'https://mingchang-production.up.railway.app',
+    'https://demoshop.up.railway.app',
 ])
 
 # Database - PostgreSQL on Railway
